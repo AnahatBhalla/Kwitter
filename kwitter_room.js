4a784ff9bdeac625673fa05884ff959c8eec7e40
AnahatBhalla <100200205+AnahatBhalla@users.noreply.github.com>
2023-04-11 19:02:50 +0800

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -4,7 +4,29 @@
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Start code
-
+row="<div class='room_name' id="+Room_names+" onclick='redirecttoroomname(this.id)' >#"+Room_names+"</div><hr>";
+document.getElementById("output").innerHTML+=row;
       //End code
       });});}
 getData();
+
+function addRoom(){
+      room_name= document.getElementById("room_name").ariaValueMax;
+      firebase.database().ref("/").child(room_name).update({
+            purpose:"adding room name"
+      });
+      localStorage.setItem("room_name",room_name);
+      window.location="kwitter_page.html";
+
+}
+
+function logout(){
+localStorage.removeItem("room_name");
+localStorage.removeItem("user_name");
+window.location="index.html";
+}
+
+function redirecttoroomname(name){
+localStorage.setItem("room_name",name);
+window.location="twitter_page.html";
+}
